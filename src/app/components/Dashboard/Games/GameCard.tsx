e82a5c382e9df16bc910d22eb640c589d84c9a2e
lostfabi <fabianba473@gmail.com>
2025-11-08 22:44:51 +0100

    GameCard redesign

--- src/app/components/Dashboard/Games/GameCard.tsx
+++ src/app/components/Dashboard/Games/GameCard.tsx
@@ -4,8 +4,11 @@ import { Game } from "@/app/lib/definitions";
 import { Chess } from "chess.js";
 import { Chessboard } from "react-chessboard";
 import Link from "next/link";
+import {splitResult} from "@/app/lib/helperFunctions";
 
 export default function ({ game, tournamentName }: { game: Game, tournamentName: string }) {
+    const scores = splitResult(game.result)
+
     const chess = new Chess()
 
     try {
@@ -17,12 +20,18 @@ export default function ({ game, tournamentName }: { game: Game, tournamentName:
         <div>
             <Link href={`/dashboard/Games/${game.id}`}>
                 <div className="bg-light-secondary/40 dark:bg-dark-secondary/40 hover:bg-light-secondary/60 hover:dark:bg-dark-secondary/60 py-2 px-3 rounded-md">
-                    <div className="pb-2">
+                    <div className="pb-2 font-bold">
                         <p>{tournamentName}</p>
                     </div>
-                    <p>{game.playerBlack}</p>
+                    <div className="flex flex-row items-center justify-between pr-1">
+                        <p>{game.playerBlack}</p>
+                        <p>{scores.black}</p>
+                    </div>
                     <Chessboard options = {{showNotation: false, position: chess.fen()}}/>
-                    <p>{game.playerWhite}</p>
+                    <div className="flex flex-row items-center justify-between pr-1">
+                        <p>{game.playerWhite}</p>
+                        <p>{scores.white}</p>
+                    </div>
                 </div>
             </Link>
         </div>
