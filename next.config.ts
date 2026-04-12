ebecb3b4ada551197634a09aaf21522a8203096b
lostfabi <fabianba473@gmail.com>
2026-04-12 12:18:13 +0200

    kp

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  turbopack:  {
+    /*
+    turbopack:  {
       root: "C:\\Users\\fabia\\Desktop\\Informatik\\Projekte\\chess\\untitled"
   }
+*/
 };
 
 export default nextConfig;
